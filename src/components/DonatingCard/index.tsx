348779f7ba5d9d8dc81c06b988b672be66441810
Xiao Qingyu <ixiaoqy@gmail.com>
2024-07-17 02:53:40 +0800

    Update index.tsx

--- src/components/DonatingCard/index.tsx
+++ src/components/DonatingCard/index.tsx
@@ -2,9 +2,6 @@ import { AmountImageMap } from './AmountQrMap'
 import { Amount } from './components/Amount'
 import { useEffect, useState } from 'react'
 
-export type AmountType = -1 | 6 | 12 | 36 | 50 | 66
-const displayAmount: AmountType[] = [6, 12, 36, 50, 66, -1]
-
 export const DonatingCard = ({ className, onAmountChange }: { className?: string; onAmountChange?: (amount: AmountType) => void }) => {
   const [amount, setAmount] = useState<AmountType | undefined>(undefined)
 

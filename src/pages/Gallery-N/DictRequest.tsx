b17d39a9820569e3bdbc97e3f1d251656e09a0da
Xiao Qingyu <ixiaoqy@gmail.com>
2024-07-17 03:28:23 +0800

    Update DictRequest.tsx

--- src/pages/Gallery-N/DictRequest.tsx
+++ src/pages/Gallery-N/DictRequest.tsx
@@ -25,23 +25,10 @@ export default function DictRequest() {
           onClose={onClosePanel}
         >
           <p className="text-sm text-gray-600 dark:text-gray-300">
-            如果您有相关的编程基础，可以参考
-            <a
-              href="https://github.com/Kaiyiwing/qwerty-learner/blob/master/docs/toBuildDict.md"
-              className="px-2 text-blue-500"
-              target="_blank"
-              rel="noreferrer"
-            >
-              导入词典
-            </a>
-            ，给项目贡献新的词典。
-            <br />
-            <br />
             如果您没有相关的编程基础，可以将您的字典需求发送邮件到{' '}
-            <a href="mailto:me@kaiyi.cool" className="px-2 text-blue-500" aria-label="发送邮件到 me@kaiyi.cool">
-              me@kaiyi.cool
+            <a href="mailto:ixiaoqy@outlook.com" className="px-2 text-blue-500" aria-label="发送邮件到 ixiaoqy@outlook.com">
+              ixiaoqy@outlook.com
             </a>
-            ，或者在网页底部添加我们的用户社群进行反馈。
           </p>
           <br />
         </InfoPanel>

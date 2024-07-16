bd48436b1ce4b6f01a30de524687747f6cc596f6
Xiao Qingyu <ixiaoqy@gmail.com>
2024-07-17 01:44:43 +0800

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -68,7 +68,8 @@ const Footer: React.FC = () => {
       </InfoPanel>
       
       <footer className="mb-1 mt-4 flex w-full items-center justify-center gap-2.5 text-sm ease-in" onClick={(e) => e.currentTarget.blur()}>
-        
+
+        <p>Power by</p>
         <button
           className="cursor-pointer text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
           type="button"

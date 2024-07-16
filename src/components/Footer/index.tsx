23582719ff59390f9f8ab96e6198b89c918c6f04
Xiao Qingyu <ixiaoqy@gmail.com>
2024-07-17 02:17:05 +0800

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -41,19 +41,8 @@ const Footer: React.FC = () => {
       
       <footer className="mb-1 mt-4 flex w-full items-center justify-center gap-2.5 text-sm ease-in" onClick={(e) => e.currentTarget.blur()}>
 
-        <p>Power by</p>
-        <button
-          className="cursor-pointer text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
-          type="button"
-          onClick={(e) => {
-            handleOpenInfoPanel('donate')
-            e.currentTarget.blur()
-          }}
-        >
-          @ 白小七羽
-        </button>
+        <p>Power by@ 白小七羽</p>
       </footer>
-    </>
   )
 }
 

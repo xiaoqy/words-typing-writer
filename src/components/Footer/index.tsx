33c6191ed98bc0394e8e51c53940931e760993b4
Xiao Qingyu <ixiaoqy@gmail.com>
2024-07-17 02:34:16 +0800

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -21,51 +21,6 @@ import IconCoffee from '~icons/tabler/coffee'
 import IconTerminal2 from '~icons/tabler/terminal-2'
 import IconFlagChina from '~icons/twemoji/flag-china'
 
-const Footer: React.FC = () => {
-  const [infoPanelState, setInfoPanelState] = useAtom(infoPanelStateAtom)
-
-  const handleOpenInfoPanel = useCallback(
-    (modalType: InfoPanelType) => {
-      recordOpenInfoPanelAction(modalType, 'footer')
-      setInfoPanelState((state) => ({ ...state, [modalType]: true }))
-    },
-    [setInfoPanelState],
-  )
-
-  const handleCloseInfoPanel = useCallback(
-    (modalType: InfoPanelType) => {
-      setInfoPanelState((state) => ({ ...state, [modalType]: false }))
-    },
-    [setInfoPanelState],
-  )
-      
-      return (
-    <>
-      <InfoPanel
-        openState={infoPanelState.donate}
-        title="Buy us a coffee"
-        icon={IconCoffee}
-        buttonClassName="bg-amber-500 hover:bg-amber-400"
-        iconClassName="text-amber-500 bg-amber-100 dark:text-amber-300 dark:bg-amber-500"
-        onClose={() => handleCloseInfoPanel('donate')}
-      >
-        <p className="indent-4 text-sm text-gray-500 dark:text-gray-300">
-          非常感谢大家使用 白羽单词打字机，目前该网站使用业余时间在维护，为了保证网站能够持续地提供给大家高质量的服务，我们需要您的帮助！
-          <br />
-          您的捐款将有助于我们支付网站的运营成本，改进网站的功能和设计，并提高用户体验。
-          <br />
-        </p>
-        <br />
-        <p className="indent-4 text-sm text-gray-700 dark:text-gray-200">
-          我们相信，共同的努力可以让 白羽单词打字机 成为更好的学习平台，也相信您的支持将给予我们持续前进的动力。 感谢您的支持！
-        </p>
-        <br />
-        <div className="flex items-center justify-center py-2">
-          <StickerButton className="" />
-        </div>
-
-        <DonatingCard />
-      </InfoPanel>
       <footer className="mb-1 mt-4 flex w-full items-center justify-center gap-2.5 text-sm ease-in" onClick={(e) => e.currentTarget.blur()}>
         <p>Powered by 白小七羽</p>
       </footer>

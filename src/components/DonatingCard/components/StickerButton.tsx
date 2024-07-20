99efdc1333b3e954dcccf26c09091b74c87c21c8
Xiao Qingyu <ixiaoqy@gmail.com>
2024-07-20 23:54:56 +0800

    Update StickerButton.tsx

--- src/components/DonatingCard/components/StickerButton.tsx
+++ src/components/DonatingCard/components/StickerButton.tsx
@@ -2,8 +2,6 @@ import sticker2 from '@/assets/sticker2.jpg'
 import sticker1 from '@/assets/sticker.jpg'
 import { Tooltip } from 'react-tooltip'
 import 'react-tooltip/dist/react-tooltip.css'
-import IconInfo from '~icons/ic/outline-info'
-import SolarStickerSmileSquareOutline from '~icons/solar/sticker-smile-square-outline'
 
 export const StickerButton = ({ className }: { className?: string }) => {
   return (
@@ -17,21 +15,6 @@ export const StickerButton = ({ className }: { className?: string }) => {
         <SolarStickerSmileSquareOutline className="mb-[2px] mr-2 inline-block text-sm " />
         查看贴纸
       </div>
-      <Tooltip
-        id="sticker-btn"
-        opacity={1}
-        className="flex flex-col items-center justify-center gap-2 rounded-lg !bg-gray-100 !p-4 shadow-lg shadow-gray-300 
-        dark:!bg-gray-800 dark:text-gray-300 dark:shadow-lg dark:shadow-gray-700"
-      >
-        <div className="flex gap-2">
-          <img src={sticker1} alt="alipay" className=" h-44 rounded shadow-lg shadow-gray-300 dark:shadow-gray-700" />
-          <img src={sticker2} alt="alipay" className=" h-44 rounded shadow-lg shadow-gray-300 dark:shadow-gray-700" />
-        </div>
-        <span className="mt-3 text-xs font-bold text-gray-500">
-          <IconInfo className="mb-[3px] mr-1 inline-block" />
-          此贴纸非商品，仅用于感谢您的捐赠，不可用于任何商业用途
-        </span>
-      </Tooltip>
     </>
   )
 }

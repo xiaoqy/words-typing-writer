8303e814839ca3e794da09a398de68f881f3ccf7
unknown <ixiaoqy@gmail.com>
2024-08-16 01:20:37 +0800

    Update DictRequest.tsx

--- src/pages/Gallery-N/DictRequest.tsx
+++ src/pages/Gallery-N/DictRequest.tsx
@@ -18,17 +18,15 @@ export default function DictRequest() {
       {showPanel && (
         <InfoPanel
           openState={showPanel}
-          title="申请词典"
+          title="对词典有疑问？"
           icon={IconBook2}
           buttonClassName="bg-indigo-500 hover:bg-indigo-400"
           iconClassName="text-indigo-500 bg-indigo-100 dark:text-indigo-300 dark:bg-indigo-500"
           onClose={onClosePanel}
         >
           <p className="text-sm text-gray-600 dark:text-gray-300">
-            如果您没有相关的编程基础，可以将您的字典需求发送邮件到{' '}
-            <a href="mailto:ixiaoqy@outlook.com" className="px-2 text-blue-500" aria-label="发送邮件到 ixiaoqy@outlook.com">
-              ixiaoqy@outlook.com
-            </a>
+            如果您遇到词典错误的问题，或新增词典内容的建议。
+            请将您的需求发送邮件到 <a href="mailto:ixiaoqy@outlook.com" className="px-2 text-blue-500" aria-label="发送邮件到 ixiaoqy@outlook.com">ixiaoqy@outlook.com</a>
           </p>
           <br />
         </InfoPanel>
